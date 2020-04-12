fd450ead729bfe193bc3b51804052fa280fe8b38
usmanmazher1 <63006057+usmanmazher1@users.noreply.github.com>
2020-04-12 04:19:56 -0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,26 @@
+﻿using JSon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static Student[] empList = new Student[]
+{
+new Student() {Name="Fawad Bin Tariq", Regno="bse173091" , Cgpa=3.2 },
+new Student() {Name="Muhammad Usman", Regno="bse173090" , Cgpa=3.1 },
+new Student() {Name="Samir Obaid", Regno="bse173092" , Cgpa=3.3 },
+new Student() {Name="Omaid Ghayur",Regno="bse173093" , Cgpa=3.4 },
+};
+
+
+        public JsonResult GetEmpData()
+        {
+            return Json(empList);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace JSon.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public String Regno { get; set; }
+        public int Cgpa { get; set; }
+    }
+}
